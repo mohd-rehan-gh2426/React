f603bc473c3829e64779d64a7b12d7f1000cd36d
Mohd Rehan <mdrehanchhipa@gmail.com>
2025-10-23 09:21:24 +0530

    building colour explorer

---
+++ 2.namaste_dev_questions/2.Color_Explorer/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Color Explorer</h1>
+      <div className='input-section'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
